d6170f581e13da85e6fd007f1bf40c70acac3520
uselesswhale1 <daniashevchenko@gmail.com>
2021-11-08 21:05:10 +0200

    first-react-app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const element = <h1>Hello World меня зовут: Даниил Шевченко</h1>
+
+console.log(element)
+
+ReactDOM.render(element, document.getElementById('root'))
